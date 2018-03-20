580df6f1d286ef583a7a568ef4147fb5c7234922
jonathan MONBEIG <jonathan.mbg@hotmail.com>
2018-03-20 16:57:33 +0100

    Ajout schema, read et save

--- src/stockage/Schema.java
+++ src/stockage/Schema.java
@@ -1,5 +1,44 @@
 package stockage;
 
-public class Schema {
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Dictionary;
+import stockage.type.Type;
 
+
+public class Schema {
+	private Dictionary<String, Type> attributs;
+	
+	public Schema() {
+		super();
+	}
+	
+	public Schema(Dictionary<String, Type> attributs) {
+		super();
+		this.attributs = attributs;
+	}
+	
+	public Schema(String fileName) throws FileNotFoundException {
+		Schema schema = null;
+	    XMLDecoder decoder = new XMLDecoder(new FileInputStream(fileName));
+	    try {
+	    	this.attributs = ((Schema)decoder.readObject()).attributs;
+	    } finally {
+	        decoder.close();
+	    }
+	}
+	
+	public void saveSchema(String fileName) throws FileNotFoundException {
+		XMLEncoder encoder = new XMLEncoder(new FileOutputStream(fileName));
+        try {
+            encoder.writeObject(this);
+            encoder.flush();
+        } finally {
+            encoder.close();
+        }
+	}
+	
 }
