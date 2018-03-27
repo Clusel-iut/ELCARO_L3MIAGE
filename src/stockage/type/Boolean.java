5021a5d3ed398978fd3273868c191531a42679aa
florian PIGNARD <florian.pignard@etu.univ-grenoble-alpes.fr>
2018-03-27 17:13:00 +0200

    Type Boolean.

--- src/stockage/type/Boolean.java
+++ src/stockage/type/Boolean.java
@@ -7,13 +7,19 @@ import java.io.IOException;
 public class Boolean extends Type<java.lang.Boolean> {
 
 	@Override
-	protected void write(DataOutputStream os, java.lang.Boolean valeur) throws IOException {
+	public void write(DataOutputStream os, java.lang.Boolean valeur) throws IOException {
 		os.writeBoolean(valeur);		
 	}
 
 	@Override
-	protected java.lang.Boolean read(DataInputStream is) throws IOException {
+	public java.lang.Boolean read(DataInputStream is) throws IOException {
 		return is.readBoolean();
+	}
+
+	@Override
+	public java.lang.Boolean parse(String data) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
